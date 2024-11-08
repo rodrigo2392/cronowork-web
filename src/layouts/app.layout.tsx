276150810bc92129f71c0a5fc738ba02c91f875c
Rodrigo Méndez <91278835+rodrigo2392@users.noreply.github.com>
2024-11-07 18:10:34 -0600

    chore: format fixed

--- src/layouts/app.layout.tsx
+++ src/layouts/app.layout.tsx
@@ -1,22 +1,22 @@
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ReactElement } from "react";
-import AppBarItem from "../components/AppBar";
-import DrawerComponent from "../components/Drawer";
-import Main from "../components/Main";
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ReactElement } from 'react'
+import AppBarItem from '../components/AppBar'
+import DrawerComponent from '../components/Drawer'
+import Main from '../components/Main'
 
 export default function AppLayout({ children }: { children: ReactElement }) {
   return (
     <>
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          overflow: "hidden",
-          backgroundColor: "#f0f2f5",
+          display: 'flex',
+          flexDirection: 'column',
+          overflow: 'hidden',
+          backgroundColor: '#f0f2f5',
         }}
       >
-        <Box sx={{ display: "flex" }}>
+        <Box sx={{ display: 'flex' }}>
           <CssBaseline />
           <AppBarItem
             isDrawerOpen={true}
@@ -28,7 +28,7 @@ export default function AppLayout({ children }: { children: ReactElement }) {
           />
         </Box>
         <Main open={true}>
-          <Box
+             <Box
             sx={{
               marginBottom: 4,
               paddingLeft: { xs: 2, md: 5, lg: 10 },
@@ -40,5 +40,5 @@ export default function AppLayout({ children }: { children: ReactElement }) {
         </Main>
       </Box>
     </>
-  );
+  )
 }
--- src/validation/auth.validation.ts
+++ src/validation/auth.validation.ts
@@ -1,31 +1,31 @@
-import { object, string, ref } from "yup";
+import { object, string, ref } from 'yup'
 
 export interface loginRequest {
-  email: string;
-  password: string;
+  email: string
+  password: string
 }
 
 export interface registerRequest {
-  email: string;
-  password: string;
-  name: string;
+  email: string
+  password: string
+  name: string
 }
 
 export const loginSchema = object({
-  password: string().required("El campo de contraseña es requerido"),
+  password: string().required('El campo de contraseña es requerido'),
   email: string()
-    .email("Debes ingresar un email válido")
-    .required("El campo de email es requerido"),
-});
+    .email('Debes ingresar un email válido')
+    .required('El campo de email es requerido'),
+})
 
 export const registerSchema = object({
-  password: string().required("El campo de contraseña es requerido"),
+  password: string().required('El campo de contraseña es requerido'),
   repeat_password: string().oneOf(
-    [ref("password")],
-    "Las contraseñas deben coincidir",
+    [ref('password')],
+    'Las contraseñas deben coincidir',
   ),
   email: string()
-    .email("Debes ingresar un email válido")
-    .required("El campo de email es requerido"),
-  name: string().required("El campo de nombre es requerido"),
-});
+    .email('Debes ingresar un email válido')
+    .required('El campo de email es requerido'),
+  name: string().required('El campo de nombre es requerido'),
+})
